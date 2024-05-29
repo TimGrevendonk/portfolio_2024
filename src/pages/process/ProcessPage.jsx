a769a1348ef685d4d54ae9bfa0367e2ee25042e8
TimGrevendonk <r0831309@student.thomasmore.be>
2024-05-29 03:35:16 +0200

    process done

--- src/pages/process/ProcessPage.jsx
+++ src/pages/process/ProcessPage.jsx
@@ -31,7 +31,7 @@ export function ProcessPage({ ...props }) {
                             </p>
                         </div>
                         <figure>
-                            <img src={images(`./${"workspot-mediacafe"}.jpg`)} alt="" />
+                            <img src={images(`./${"workspot-mediacafe"}.jpg`)} alt="workspace" />
                             <figcaption>
                                 <h2>Great workspots</h2>
                                 <p>And the plants</p>
@@ -48,10 +48,10 @@ export function ProcessPage({ ...props }) {
                     </div>
                     <article>
                         <figure>
-                            <img src={images(`./${"me-train"}.jpg`)} alt="" />
+                            <img src={images(`./${"word_to_colour_psycology"}.jpg`)} alt="colours for words" />
                             <figcaption>
-                                <h2>hello</h2>
-                                <p>to you</p>
+                                <h2>Adjectives</h2>
+                                <p>important words</p>
                             </figcaption>
                         </figure>
                         <div>
@@ -95,10 +95,10 @@ export function ProcessPage({ ...props }) {
                             </p>
                         </div>
                         <figure>
-                            <img src={images(`./${"workspot-mediacafe"}.jpg`)} alt="" />
+                            <img src={images(`./${"inspiring_images"}.jpg`)} alt="a lot of images" />
                             <figcaption>
-                                <h2>Great workspots</h2>
-                                <p>And the plants</p>
+                                <h2>A mix of images</h2>
+                                <p>A fine selection</p>
                             </figcaption>
                         </figure>
                     </article>
@@ -112,10 +112,10 @@ export function ProcessPage({ ...props }) {
                     </div>
                     <article>
                         <figure>
-                            <img src={images(`./${"me-train"}.jpg`)} alt="" />
+                            <img src={images(`./${"moodboard"}.jpg`)} alt="" />
                             <figcaption>
-                                <h2>hello</h2>
-                                <p>to you</p>
+                                <h2>A moodboard</h2>
+                                <p>Telling the vibes</p>
                             </figcaption>
                         </figure>
                         <div>
@@ -159,10 +159,199 @@ export function ProcessPage({ ...props }) {
                             </p>
                         </div>
                         <figure>
-                            <img src={images(`./${"workspot-mediacafe"}.jpg`)} alt="" />
+                            <img src={images(`./${"extracting_colours"}.jpg`)} alt="" />
                             <figcaption>
-                                <h2>Great workspots</h2>
-                                <p>And the plants</p>
+                                <h2>Colour extraction</h2>
+                                <p>The off colours too</p>
+                            </figcaption>
+                        </figure>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.define} ${styles.containerSection} mountain-blue`}>
+                <div className={`${styles.widthSetter} `}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>Wireframe</h1>
+                        <p>gray, greyish, and great.</p>
+                    </div>
+                    <article>
+                        <figure>
+                            <img src={images(`./${"wireframe_hoppin"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>Screens and components</h2>
+                                <p>no colours included</p>
+                            </figcaption>
+                        </figure>
+                        <div>
+                            <h2>Layout and conceptualizing</h2>
+                            <p className='popout'>
+                                With wireframes we can get a feeling of layout and find the first oversights in design and form. it is a quick way without wasting toà much time picking on the details.
+                            </p>
+                            <p className='popout'>
+                                Texts can be placeholders or preferably just different grey squares. It signifies the spacing and size of each element. Images and icons are not filled in, but the space is reserved by shapes with big crosses in them.
+                            </p>
+                            <p className='popout'>
+                                A good estimation of the amount of needed screens is gathered from creating wireframes. They do not focus on the details like different modals or CRUD actions.
+                            </p>
+                            <p className='popout'>
+                                It is sometimes more important to keep it minimalistic. Texts or details can sometimes distract from the purpose of a good wireframe.
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.introduction} ${styles.containerSection}`}>
+                <div className={`${styles.widthSetter}`}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>Prototype</h1>
+                        <p>adding those colours.</p>
+                    </div>
+                    <article>
+                        <div>
+                            <h2>Look and feel</h2>
+                            <p className='popout'>
+                                A prototype is the first steps into a a clickable and interactive design. It is the first time we can see the design in action and feel the flow of the project.
+                            </p>
+                            <p className='popout'>
+                                The prototype is a good way to test the design and layout. It is also a good first view to see how the colours and elements work together.
+                            </p>
+                            <p className='popout'>
+                                Here is the first time images are used to see how they blend in with other elements. A prototype gives a good representation of how the view is scaled according to the used elements.
+                            </p>
+                            <p className='popout'>
+                                In a prototype the details of interaction can be worked out as reference in development and user control.
+                            </p>
+                        </div>
+                        <figure>
+                            <img src={images(`./${"prototype_portfolio_home"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>Protoypes</h2>
+                                <p>Do you recognise these!?</p>
+                            </figcaption>
+                        </figure>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.define} ${styles.containerSection} mountain-blue`}>
+                <div className={`${styles.widthSetter} `}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>Testing</h1>
+                        <p>Mom can you do this...</p>
+                    </div>
+                    <article>
+                        <figure>
+                            <img src={images(`./${"hoppin_live_test"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>User test</h2>
+                                <p>How does this work</p>
+                            </figcaption>
+                        </figure>
+                        <div>
+                            <h2>Input and feedback</h2>
+                            <p className='popout'>
+                                The input of a client or end-user is the most important part of a good project. With feedback we can find oversights that where not easily found in development.
+                            </p>
+                            <p className='popout'>
+                                Testing can be done via live user test or simply sending the design files to a friend or family member that has no knowhouw of the project. there are tools that support detailed testing insights.
+                            </p>
+                            <p className='popout'>
+                                The speed and ease of acces can give a good indication as to where there are bottleneks in the design. Assumptions can be made on how the user will interact with the design, but a user will always surprise you.
+                            </p>
+                            <p className='popout'>
+                                Testing is not done once in a single stage, it is a continues process and done in many forms.
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.introduction} ${styles.containerSection}`}>
+                <div className={`${styles.widthSetter}`}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>Programming</h1>
+                        <p>make it look easy.</p>
+                    </div>
+                    <article>
+                        <div>
+                            <h2>Problem to product</h2>
+                            <p className='popout'>
+                                Choosing the right technologies is key to a working end result. All the preperation is for nothing if the wrong tools are used. 
+                            </p>
+                            <p className='popout'>
+                                A framewok helps out in a lot of ways but can be cumbersome in other ways. The desision to go more lightweight depends on the project.
+                            </p>
+                            <p className='popout'>
+                                Programming based on the designs gives a enormous speed boost in development. The designs are the blueprint and the programmed result is the construction.
+                            </p>
+                            <p className='popout'>
+                                Responsive pages and dynamically loaded elements are practices of the present. The "works on my machine" is sometimes the hardest problem to work around. 
+                            </p>
+                        </div>
+                        <figure>
+                            <img src={images(`./${"me_programming"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>A programmer</h2>
+                                <p>Meticulously programming</p>
+                            </figcaption>
+                        </figure>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.define} ${styles.containerSection} mountain-blue`}>
+                <div className={`${styles.widthSetter} `}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>Extras</h1>
+                        <p>there was more.</p>
+                    </div>
+                    <article>
+                        <figure>
+                            <img src={images(`./${"lessons_school"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>Learning course</h2>
+                                <p>Happy times</p>
+                            </figcaption>
+                        </figure>
+                        <div>
+                            <h2>Important and necessary</h2>
+                            <p className='popout'>
+                                Other topics that are important and deserve mentioning:
+                            </p>
+                            <p className='popout'>
+                                Typography is an important part of a design and can give a different feeling to a product. The cycle of finding the right typography is done in the setops of finding inspiration, making moodboards, and colours, they are al connected. 
+                            </p>
+                            <p className='popout'>
+                                Some projects need extensive branding and the creation of a logo. this could even come as late as the prototype stage. Branding provides help for colours and typography.
+                            </p>
+                            <p className='popout'>
+                                A final product must stay alive of course. A way to make it available to the public i not always easy. A side project can not sustain a high costed hosting platform.
+                            </p>
+                        </div>
+                    </article>
+                </div>
+            </section>
+            <section className={`${styles.introduction} ${styles.containerSection}`}>
+                <div className={`${styles.widthSetter}`}>
+                    <div className={`${styles.titleSection}`}>
+                        <h1>reflecting</h1>
+                        <p>never making faults again.</p>
+                    </div>
+                    <article>
+                        <div>
+                        <h2>Input and feedback</h2>
+                            <p className='popout'>
+                                There is always something you learn, some stupid mistake you made that you will never forget now (until you do it again).
+                            </p>
+                            <p className='popout'>
+                                Every project and experience is a learning process. the more we do and experiment the better we get.
+                            </p>
+                            <p className='popout'>
+                                Sometimes it is good to look back at the finished project before moving to the next. take every step as a victory, no matter how hard the journy.
+                            </p>
+                        </div>
+                        <figure>
+                            <img src={images(`./${"dorm_nighttime"}.jpg`)} alt="" />
+                            <figcaption>
+                                <h2>After a project</h2>
+                                <p>Goodnight</p>
                             </figcaption>
                         </figure>
                     </article>
