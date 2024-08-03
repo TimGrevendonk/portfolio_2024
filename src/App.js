53f8c9752ef19b79de4fb51f694cbaa7a97eddc2
TimGrevendonk <r0831309@student.thomasmore.be>
2024-08-03 23:40:16 +0200

    google analytics

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { ProjectsPage } from "pages/projects/ProjectsPage";
 import { ProjectDetailPage } from "pages/projects/project/ProjectDetailPage";
 import { ProcessPage } from "pages/process/ProcessPage";
 import { ErrorPage } from "pages/error/ErrorPage";
-
+import ReactGA from 'react-ga4';
 import './App.css';
 import 'styles/globals.scss';
 
+ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
+
 function App() {
-  return (
-    
+
+  return (    
       <BrowserRouter basename="/portfolio_2024">
         <Routes>
           <Route path="/" element={<MainLayout />}>
